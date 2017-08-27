0263df5dec12416dca5bbaab4dfa90ecf58c3eef
anzcarroll <anzcarroll@gmail.com>
2017-08-27 09:41:42 -0400

    toy app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
+  get '/users', to: 'users#index'
+  get '/users/1' to: 'users#'
 end
